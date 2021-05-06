db3275d4bf3dd05b674bd8bf860a71e057626762
Nils <Nils-Arne.Abbing@adesso.de>
2021-05-06 14:33:10 +0200

    Adding Spring Security Classes, Login View and Admin Panel Site

---
+++ src/main/java/de/adesso/jani/Security/ConfigureUIServiceInitListener.java
@@ -0,0 +1,41 @@
+package de.adesso.jani.Security;
+
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import de.adesso.jani.views.Security.LoginView;
+import de.adesso.jani.views.main.MainView;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class ConfigureUIServiceInitListener implements VaadinServiceInitListener {
+
+    private ArrayList<Class> excludedClasses = new ArrayList<>();
+
+    private void setExcludedClasses(){
+        addNoSec(MainView.class);
+    }
+
+    private void addNoSec(Class classname){
+        excludedClasses.add(classname);
+    }
+
+    @Override
+    public void serviceInit(ServiceInitEvent event) {
+        event.getSource().addUIInitListener(uiEvent -> {
+            final UI ui = uiEvent.getUI();
+            ui.addBeforeEnterListener(this::authenticateNavigation);
+        });
+    }
+
+    private void authenticateNavigation(BeforeEnterEvent event) {
+        if (!LoginView.class.equals(event.getNavigationTarget())
+                && !SecurityUtils.isUserLoggedIn()) {
+            event.rerouteTo(LoginView.class);
+        }
+    }
+}
