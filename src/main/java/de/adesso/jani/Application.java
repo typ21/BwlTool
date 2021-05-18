cccc5e2d50bc03c452883edee2ba7b9c3e8fa719
Jan Wojtasik <jan.wojtasik@adesso.de>
2021-05-18 14:15:53 +0200

    Changed system for Account-Database

--- src/main/java/de/adesso/jani/Application.java
+++ src/main/java/de/adesso/jani/Application.java
@@ -1,6 +1,7 @@
 package de.adesso.jani;
 
 import com.vaadin.flow.server.VaadinServletConfiguration;
+import de.adesso.jani.SimpleDatabase.AdminStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -14,8 +15,10 @@ import org.vaadin.artur.helpers.LaunchUtil;
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+        System.out.println(AdminStorage.addUser("admin", "deinemudda"));
         //LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
         SpringApplication.run(Application.class, args);
+
     }
 
 }
--- src/main/java/de/adesso/jani/SimpleDatabase/AdminStorage.java
+++ src/main/java/de/adesso/jani/SimpleDatabase/AdminStorage.java
@@ -1,40 +1,112 @@
 package de.adesso.jani.SimpleDatabase;
 
-import java.io.File;
-import java.io.IOException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdminStorage {
 
-    private static final File DATABASE_FILE = new File("/database/Admins.txt");
-    private static final long offset = 0xABCDEFAB;
+    private static final File DATABASE_HEAD = new File("./database/");
+    private static final char offset = 0x3E;
+
+    private static HashMap<String, String> accounts;
 
 
     static {
 
         boolean existing = true;
 
-        if(!DATABASE_FILE.getParentFile().isDirectory()){
-            existing = DATABASE_FILE.getParentFile().mkdirs();
-        }
-        if(!DATABASE_FILE.exists()){
-            try {
-               existing &= DATABASE_FILE.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        if(!DATABASE_HEAD.getParentFile().isDirectory()){
+            existing = DATABASE_HEAD.getParentFile().mkdirs();
         }
 
         if(!existing) {
             System.out.println("Konnte Datenbank nicht erstellen - terminiere!");
             System.exit(-1);
         }
+
+        try {
+            accounts = readFiles();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Konnte Datenbank nicht lesen - terminiere!");
+            System.exit(-1);
+        }
+        System.out.println("Datenbank initialisiert!");
+    }
+
+    private static HashMap<String, String> readFiles() throws IOException {
+
+        HashMap<String, String> inter = new HashMap<String, String>();
+
+        File[] allAccounts = DATABASE_HEAD.listFiles();
+
+        if(allAccounts != null) {
+            for (File f : allAccounts) {
+
+                String name = f.getName();
+
+                BufferedReader br = new BufferedReader(new FileReader(f));
+
+                StringBuilder sb = new StringBuilder();
+
+                br.lines().forEach(sb::append);
+
+                accounts.put(name, sb.toString());
+            }
+        }
+
+        return inter;
+    }
+
+    private static boolean writeConfig() {
+
+        boolean generalSuccess = true;
+
+        try {
+            for(Map.Entry<String, String> current : accounts.entrySet()) {
+
+                File currentFile = new File(DATABASE_HEAD, current.getKey());
+
+                boolean success = true;
+
+                if(currentFile.exists())
+                    success = currentFile.delete();
+
+                success &= currentFile.createNewFile();
+
+                FileWriter fw = new FileWriter(currentFile);
+
+                fw.write(current.getValue());
+
+                fw.close();
+
+                generalSuccess &= success;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return generalSuccess;
     }
 
     public static String getPasswordForName(String s) {
-        return "";
+        return contains(s) ? accounts.get(s) : null;
     }
 
     public static boolean contains(String s) {
-        return true;
+        return accounts.containsKey(s);
+    }
+
+    public static boolean addUser(String name, String pass) {
+
+        pass = new BCryptPasswordEncoder(10).encode(pass);
+
+        accounts.put(name, pass);
+
+        return writeConfig();
+
     }
 }
