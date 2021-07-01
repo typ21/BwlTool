ceb0ca9dc2a5d236dc6a66feb748b8e03bd8d57d
Abbing, Nils Arne <nils-Arne.Abbing@adesso.de>
2021-07-01 13:14:49 +0200

    Erste Anfragen an die Datenbank, die meisten funktionieren noch nicht richtig

--- src/main/java/de/adesso/jani/views/sites/Interfaces/AdminPanelInterface.java
+++ src/main/java/de/adesso/jani/views/sites/Interfaces/AdminPanelInterface.java
@@ -1,18 +1,28 @@
 package de.adesso.jani.views.sites.Interfaces;
 
 import com.vaadin.flow.component.charts.model.DataSeries;
+import com.vaadin.flow.component.charts.model.DataSeriesItem;
 import com.vaadin.flow.component.charts.model.ListSeries;
 import de.adesso.jani.backend.DataService;
+import de.adesso.jani.backend.TimespanDataService;
+import de.adesso.jani.backend.Util.TimeSpanData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+
+import java.time.Period;
+import java.util.List;
 
 @Service
 public class AdminPanelInterface {
 
     @Autowired
     private final DataService ds;
+    @Autowired
+    private  final TimespanDataService tds;
 
-    public AdminPanelInterface(DataService ds){
+    public AdminPanelInterface(DataService ds, TimespanDataService tds){
+        this.tds = tds;
         this.ds = ds;
     }
 
@@ -31,13 +41,18 @@ public class AdminPanelInterface {
     }
 
     public DataSeries getVisitorsDay(){
+        List<Long> data = tds.getDataForTimeSpan(Period.of(0, 0, 7)).getClicksADay();
+
         DataSeries ds = new DataSeries();
         ds.setName("Besucher");
-        ds.setData(3, 8, 9, 13, 2, 0 ,7);
+        ds.setData((Number) data);
         return ds;
     }
 
     public DataSeries getVisitorsMonth(){
+        //Anfrage nicht richig
+        //List<Long> data = tds.getDataForTimeSpan(Period.of(0, 0, 7));
+
         DataSeries ds = new DataSeries();
         ds.setName("Besucher");
         ds.setData(3, 8, 9, 13, 2, 0 ,7, 4, 8, 6, 7, 9);
@@ -50,10 +65,15 @@ public class AdminPanelInterface {
     }
 
     public int getCalculationsToday() {
-        return 0;
+        TimeSpanData tsd = tds.getDataForTimeSpan(Period.of(0,0,1));
+        long cal = tsd.getCalculationsADay().stream().findFirst().get();
+        return (int) cal;
     }
 
     public int getBadCalculationsToday() {
-        return 0;
+        //Anfrage stimmt noch nicht
+        TimeSpanData tsd = tds.getDataForTimeSpan(Period.of(0,0,1));
+        long cal = tsd.getCalculationsADay().stream().findFirst().get();
+        return (int) cal;
     }
 }
