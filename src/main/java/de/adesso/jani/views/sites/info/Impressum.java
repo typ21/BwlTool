5ed8d4fe5c2c73f29230f695db22bfa21f55359b
Nils <lreg0@web.de>
2022-01-24 22:32:10 +0100

    Update Impressum.java
    generalize Impressum

--- src/main/java/de/adesso/jani/views/sites/info/Impressum.java
+++ src/main/java/de/adesso/jani/views/sites/info/Impressum.java
@@ -13,12 +13,7 @@ public class Impressum extends VerticalLayoutWithFooter {
 
     public Impressum(){
         add(
-                new H3("Angaben gemäß § 5 TMG"),
-                new Span("Arne Abbing"),
-                new Span("Eierkampstr. 34"),
-                new Span("44225 Dortmund"),
-                new H3("Kontakt"),
-                new Span("Email: lreg0@web.de")
+                //Impressum info
         );
     }
 
